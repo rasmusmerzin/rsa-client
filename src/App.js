342e17f29060854a01745e061cc71845c56c7e7e
rasmusmerzin <rasmusmerzin@gmail.com>
2020-01-12 23:49:33 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import NodeRSA from 'node-rsa';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends React.Component {
+  lenRSAOptions = [512, 1024, 2048, 4096];
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      lenRSA: 1024,
+      rsa: null
+    };
+    this.generateNewIdentity = this.generateNewIdentity.bind(this);
+  }
+
+  generateNewIdentity() {
+    this.setState({ rsa: null });
+    setTimeout(() => this.setState({ rsa: new NodeRSA({ b: this.state.lenRSA }) }));
+  }
+
+  componentDidMount() {
+    this.generateNewIdentity();
+  }
+
+  render() {
+    //document.body.className = 'dark-theme';
+    return <div id='app'>{
+      this.state.rsa === null
+        ? <div id='generating'>
+          <div>generating {this.state.lenRSA}-bit RSA key <span>( )</span></div>
+        </div>
+        : <>
+          <div className='split compact'>
+            <h1 className='row em'>Identity</h1>
+            <div>
+              <h2>Private identity</h2>
+              <div className='key'>{this.state.rsa.exportKey('pkcs8')}</div>
+            </div>
+            <div>
+              <h2>Public identity</h2>
+              <div className='key'>{this.state.rsa.exportKey('pkcs8-public')}</div>
+            </div>
+            <div className='row em sep'>
+              <button onClick={this.generateNewIdentity}>New Identity</button>
+              {this.lenRSAOptions.map(
+                b => <button>{b}</button>
+              )}
+            </div>
+            <h1 className='row em'>Encryption</h1>
+            <div>
+              <h2>Message</h2>
+              <textarea></textarea>
+            </div>
+            <div>
+              <h2>{'Recipient\'s public key'}</h2>
+              <textarea></textarea>
+            </div>
+          </div>
+        </>
+    }</div>;
+  }
 }
-
-export default App;
